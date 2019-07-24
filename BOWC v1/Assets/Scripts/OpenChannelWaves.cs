ec635e4a1ae2ee0e2b81d620b5904cb150a0ab27
kirillsil <40216566+kirillsil@users.noreply.github.com>
2019-07-24 20:34:37 +0300

    Menu scene added. RPG MMO UI Package in the "imported assets" folder

---
+++ BOWC v1/Assets/Scripts/OpenChannelWaves.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChannelWaves : MonoBehaviour
+{
+    public void OpenChannelW()
+    {
+        Application.OpenURL("https://wavesplatform.com");
+    }
+}
