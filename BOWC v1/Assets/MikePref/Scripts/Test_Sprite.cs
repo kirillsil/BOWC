fb2d164b868e85af4947f78ab0e2a550a4736b4d
Michael <updigit@gmail.com>
2019-08-26 23:06:13 +0300

    Переход на версию Юнити 2019.2.0f1

---
+++ BOWC v1/Assets/MikePref/Scripts/Test_Sprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_Sprite : MonoBehaviour
+{
+	public Sprite[] sp;
+	public Image im;
+	public int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void OnBtn(int i_)
+	{
+		i+=i_;
+		if (i<0)
+			i=0;
+		if (i>5)
+			i=5;
+		im.sprite=sp[i];
+		im.SetNativeSize();
+	}
+
+}
